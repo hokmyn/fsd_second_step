6c56624209c3e4309420539dd1bdf9c82ad1751a
Denis <den_from_bykhov1988@mail.ru>
2019-11-19 01:05:19 +0300

    added logic for datepicker

--- webpacktemplate/src/components/date-dropdown/date-dropdown.js
+++ webpacktemplate/src/components/date-dropdown/date-dropdown.js
@@ -9,23 +9,38 @@ if (!$.fn.datepicker) {
 $(".form_select-arrival, .booking-arrival").datepicker({
     startDate: new Date(),
     range: true,
-    multipleDatesSeparator: " - "
-
+    showButtonPanel: true,
+    clearButton: true,
+    navTitles: {
+        days: 'MM yyyy',
+        months: 'yyyy',
+        years: 'yyyy1 - yyyy2'
+    },
+    
+    onSelect: (formattedDate, date, inst) => {
+        if (formattedDate.split(",").length > 1) {
+            $(inst.el).val(formattedDate.split(",")[0]);
+            $(inst.el)
+                .parents(".datepicker-wrap")
+                .siblings(".datepicker-wrap")
+                .find(".input")
+                .val(formattedDate.split(",")[1]);            
+        }
+    }
 });
 
-//$(".form_select-arrival, .booking-arrival").datepicker().data('datepicker').show();
-//$(".form_select-arrival, .booking-arrival").datepicker().data('datepicker').hide();
-
-
-
-$(".date-dropdown").on("click", (e) => {   
-    if ($(e.target).hasClass("form_select-departure") || $(e.target).hasClass("booking-departure")) {
-        
+$(".date-dropdown").on("click", (e) => {
+    if ($(e.target).hasClass("form_select-departure")
+    || $(e.target).hasClass("booking-departure")
+    || $(e.target).siblings("input[class*='-departure']").length > 0
+    || $(e.target).parent().siblings("input[class*='-departure']").length > 0) {              
         $(e.target)
             .parents(".datepicker-wrap")
             .siblings(".datepicker-wrap")
             .find(".input").datepicker()
             .data('datepicker').show();
+    } else {
+        $(e.target).parents(".date-dropdown").find(".input").datepicker().data('datepicker').show();
     }
     
 });
