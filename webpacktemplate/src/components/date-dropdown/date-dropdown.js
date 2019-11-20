a3d3a6a95c9fa94caef17b7cd33f54405b83b85c
Denis <den_from_bykhov1988@mail.ru>
2019-11-21 01:10:49 +0300

    logic of datepicker is completed

--- webpacktemplate/src/components/date-dropdown/date-dropdown.js
+++ webpacktemplate/src/components/date-dropdown/date-dropdown.js
@@ -6,8 +6,16 @@ if (!$.fn.datepicker) {
     require('../../assets/css/datepicker.min.css');
 }
 
-$(".form_select-arrival, .booking-arrival").datepicker({
+$("input[class*='-arrival'], input[class*='-departure']").attr("readonly", "true");
+
+let applyButton = document.createElement('span');
+$(applyButton).addClass("select-btns__submit");
+$(applyButton).text("применить");
+
+$("input[class*='-arrival'").datepicker({
     startDate: new Date(),
+    prevHtml: '<i class="material-icons datepicker--nav-prev">arrow_back</i>',
+    nextHtml: '<i class="material-icons datepicker--nav-next">arrow_forward</i>',
     range: true,
     showButtonPanel: true,
     clearButton: true,
@@ -16,33 +24,58 @@ $(".form_select-arrival, .booking-arrival").datepicker({
         months: 'yyyy',
         years: 'yyyy1 - yyyy2'
     },
-    
+    onShow: (inst) => {
+        const elementValue = $(inst.el).val().split(",");
+        if (elementValue.length > 1) {
+            $(inst.el).val(elementValue[0]);
+            $(inst.el)
+                .parents(".datepicker-wrap")
+                .siblings(".datepicker-wrap")
+                .find("input[class*='-departure']")
+                .val(elementValue[1]);
+        }
+        if ($(".datepicker--buttons").find('.select-btns__submit').length === 0) {
+            $('.datepicker--buttons').append($(applyButton));
+        }        
+    },
     onSelect: (formattedDate, date, inst) => {
         if (formattedDate.split(",").length > 1) {
             $(inst.el).val(formattedDate.split(",")[0]);
             $(inst.el)
                 .parents(".datepicker-wrap")
                 .siblings(".datepicker-wrap")
-                .find(".input")
-                .val(formattedDate.split(",")[1]);            
+                .find("input[class*='-departure']")
+                .val(formattedDate.split(",")[1]);                        
         }
+    },
+    onHide: () => {
+        $(".select-btns__submit").remove();
+    }    
+});
+
+$("input[class*='-arrival'").datepicker().data('datepicker').$datepicker.children('.datepicker--buttons').on("click", (e) => {
+    if (e.target.dataset.action === 'clear') {
+        $("input[class*='-departure'").val("");
     }
+});
+
+$(".datepicker--buttons").on('click', (e) => {
+    if ($(e.target).hasClass("select-btns__submit")){
+        $("input[class*='-arrival'").datepicker().data('datepicker').hide();
+    }    
 });
 
 $(".date-dropdown").on("click", (e) => {
     if ($(e.target).hasClass("form_select-departure")
     || $(e.target).hasClass("booking-departure")
     || $(e.target).siblings("input[class*='-departure']").length > 0
-    || $(e.target).parent().siblings("input[class*='-departure']").length > 0) {              
+    || $(e.target).parent().siblings("input[class*='-departure']").length > 0) {
         $(e.target)
             .parents(".datepicker-wrap")
             .siblings(".datepicker-wrap")
             .find(".input").datepicker()
-            .data('datepicker').show();
-    } else {
-        $(e.target).parents(".date-dropdown").find(".input").datepicker().data('datepicker').show();
-    }
-    
+            .data('datepicker').show();        
+    }    
 });
 
 
