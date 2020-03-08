86c80ab72183272a32feb77ae7f1248bb2c01f74
Denis <den_from_bykhov1988@mail.ru>
2020-03-09 02:05:26 +0300

    correction for pp#3

--- webpacktemplate/src/components/range-slider/range-slider.js
+++ webpacktemplate/src/components/range-slider/range-slider.js
@@ -6,8 +6,8 @@ $(function () {
   $('#slider-range').slider({
     range: true,
     min: 0,
-    max: 500,
-    values: [75, 300],
+    max: 15500,
+    values: [5000, 10000],
     slide: function (event, ui) {
       $('#amount').val('$' + ui.values[0] + ' - $' + ui.values[1]);
     }
